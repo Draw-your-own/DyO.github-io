2b2799555caf028df9f59934d4794d39dfb8f77b
facuruiz1 <133668065+facuruiz1@users.noreply.github.com>
2023-05-22 12:36:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,90 +1,53 @@
-const canvas = document.querySelector(".drawing-board"),
-toolBtns = document.querySelectorAll(".tool"),
-sizeESlider = document.querySelector("#bs-slider"),
-sizeBSlider = document.querySelector("#es-slider"),
-colorBtns = document.querySelectorAll(".colors"),
-colorPicker = document.querySelector("#col"),
-clearCanvas = document.querySelector(".clear-canvas"),
-saveImg = document.querySelector(".save-image"),
-ctx = canvas.getContext("2d");
+const canvas = document.querySelector("#drawing-board");
+const ctx = canvas.getContext("2d");
 
-// global variables with default value
-let prevMouseX, prevMouseY, snapshot,
-isDrawing = false,
-selectedTool = "brush",
-brushWidth = 5,
-eraserWidth = 5,
-selectedColor = "#000";
-
-const setCanvasBackground = () => {
-    // setting whole canvas background to white, so the downloaded img background will be white
-    ctx.fillStyle = "#fff";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = selectedColor; // setting fillstyle back to the selectedColor, it'll be the brush color
-}
-
-window.addEventListener("load", () => {
-    // setting canvas width/height.. offsetwidth/height returns viewable width/height of an element
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
-    setCanvasBackground();
-});
-
-const startDraw = (e) => {
-    isDrawing = true;
-    prevMouseX = e.offsetX; // passing current mouseX position as prevMouseX value
-    prevMouseY = e.offsetY; // passing current mouseY position as prevMouseY value
-    ctx.beginPath(); // creating new path to draw
-    ctx.lineWidth = brushWidth; // passing brushSize as line width
-    ctx.strokeStyle = selectedColor; // passing selectedColor as stroke style
-    ctx.fillStyle = selectedColor; // passing selectedColor as fill style
-    // copying canvas data & passing as snapshot value.. this avoids dragging the image
-    snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
-}
-
-const drawing = (e) => {
-    if(!isDrawing) return; // if isDrawing is false return from here
-    ctx.putImageData(snapshot, 0, 0); // adding copied canvas data on to this canvas
-
-    if(selectedTool === "brush" || selectedTool === "eraser") {
-        // if selected tool is eraser then set strokeStyle to white 
-        // to paint white color on to the existing canvas content else set the stroke color to selected color
-        ctx.strokeStyle = selectedTool === "eraser" ? "#fff" : selectedColor;
-        ctx.lineTo(e.offsetX, e.offsetY); // creating line according to the mouse pointer
-        ctx.stroke(); // drawing/filling line with color
-    }
-}
-
-toolBtns.forEach(btn => {
-    btn.addEventListener("click", () => { // adding click event to all tool option
-        // removing active class from the previous option and adding on current clicked option
-        document.querySelector(".options .active").classList.remove("active");
-        btn.classList.add("active");
-        selectedTool = btn.id;
-    });
-});
-
-sizeESlider.addEventListener("change", () => eraserWidth = sizeESlider.value);
-sizeBSlider.addEventListener("change", () => brushWidth = sizeBSlider.value);// passing slider value as brushSize
-
-colorPicker.addEventListener("change", () => {
-    // passing picked color value from color picker to last color btn background
-    colorPicker.parentElement.style.background = colorPicker.value;
-    colorPicker.parentElement.click();
-});
-
-clearCanvas.addEventListener("click", () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // clearing whole canvas
-    setCanvasBackground();
-});
-
-saveImg.addEventListener("click", () => {
-    const link = document.createElement("a"); // creating <a> element
-    link.download = `${Date.now()}.jpg`; // passing current date as link download value
-    link.href = canvas.toDataURL(); // passing canvasData as link href value
-    link.click(); // clicking link to download image
-});
+let isDrawing = false;
+let selectedTool = "brush";
+let brushWidth = 5;
+let eraserWidth = 5;
+let selectedColor = "#000";
 
 canvas.addEventListener("mousedown", startDraw);
 canvas.addEventListener("mousemove", drawing);
-canvas.addEventListener("mouseup", () => isDrawing = false);
+canvas.addEventListener("mouseup", stopDraw);
+canvas.addEventListener("mouseout", stopDraw);
+
+function startDraw(e) {
+    isDrawing = true;
+    draw(e);
+}
+
+function drawing(e) {
+    if (!isDrawing) return;
+    draw(e);
+}
+
+function draw(e) {
+    const rect = canvas.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    if (selectedTool === "brush" || selectedTool === "eraser") {
+        ctx.lineWidth = selectedTool === "eraser" ? eraserWidth : brushWidth;
+        ctx.strokeStyle = selectedTool === "eraser" ? "#fff" : selectedColor;
+
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+function stopDraw() {
+    isDrawing = false;
+}
+
+// Restablecer el tamaño del lienzo de dibujo cuando cambie el tamaño de la ventana
+window.addEventListener("resize", () => {
+    const rect = canvas.getBoundingClientRect();
+    canvas.width = rect.width;
+    canvas.height = rect.height;
+});
+
+// Establecer el tamaño inicial del lienzo de dibujo
+const rect = canvas.getBoundingClientRect();
+canvas.width = rect.width;
+canvas.height = rect.height;
